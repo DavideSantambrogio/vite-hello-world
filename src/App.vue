47d5df5ee10726e13b60e78329e25ef3a9dc7e90
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-11-22 16:28:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script> 
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+
+</script>
+
+<template>
+  <AppTitle/>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      text: "La mia prima app con Vite!",
+      prova: "prova per vedere se ho capito"
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ text }}</h2>
+  <p>{{ prova }}</p>
+</template>
+
+<style></style>
